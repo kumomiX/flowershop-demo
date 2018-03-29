adbc5e2e3507231c0ce4288266034834cff22d05
kumomiX <nctilucent@gmail.com>
2018-03-30 02:18:11 +0300

    heroku

--- src/actions/flowersActions.js
+++ src/actions/flowersActions.js
@@ -15,7 +15,7 @@ const fetchFlowersError = () => ({
 })
 
 const fetchFlowers = () =>
-  fetch('http://5ab6b4a7c21b900014ab9a5a.mockapi.io/api/v1/flowers').then(
+  fetch('https://5ab6b4a7c21b900014ab9a5a.mockapi.io/api/v1/flowers').then(
     response => Promise.all([response, response.json()])
   )
 
